2e5deafec361d511b3bb0f0c3568db623f705d69
snowbeach0102 <poby262513@gmail.com>
2026-09-13 02:57:39 +0900

    2번째 배경화면 변경

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -28,9 +28,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(seedColor: Colors.blue),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: '김신영의 첫 플러터 앱'),
     );
   }
 }
